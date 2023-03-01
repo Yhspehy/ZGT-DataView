cc021dae2bbbc80ce67bbb5b9a8082598c44bbd1
Yhspehy <670395851@qq.com>
2023-03-01 15:06:02 +0800

    docs: add vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,147 @@
+import { defineConfig } from 'vitepress'
+import { version } from '../../package.json'
+import {
+  font,
+  github,
+  ogImage,
+  ogUrl,
+  releases,
+  DataViewDescription,
+  DataViewName
+} from './meta'
+
+export default defineConfig({
+  lang: 'en-US',
+  title: DataViewName,
+  description: DataViewDescription,
+  head: [
+    ['meta', { name: 'theme-color', content: '#729b1a' }],
+    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
+    [
+      'link',
+      {
+        rel: 'alternate icon',
+        href: '/favicon.ico',
+        type: 'image/png',
+        sizes: '16x16'
+      }
+    ],
+
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          'DataView, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node'
+      }
+    ],
+    ['meta', { property: 'og:title', content: DataViewName }],
+    ['meta', { property: 'og:description', content: DataViewDescription }],
+    ['meta', { property: 'og:url', content: ogUrl }],
+    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { name: 'twitter:title', content: DataViewName }],
+    ['meta', { name: 'twitter:description', content: DataViewDescription }],
+    ['meta', { name: 'twitter:image', content: ogImage }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    [
+      'link',
+      {
+        rel: 'preload',
+        as: 'style',
+        onload: "this.onload=null;this.rel='stylesheet'",
+        href: font
+      }
+    ],
+    [
+      'noscript',
+      {},
+      `<link rel="stylesheet" crossorigin="anonymous" href="${font}" />`
+    ],
+    ['link', { rel: 'mask-icon', href: '/logo.svg', color: '#ffffff' }],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/apple-touch-icon.png',
+        sizes: '180x180'
+      }
+    ]
+  ],
+  lastUpdated: true,
+  markdown: {
+    theme: {
+      light: 'vitesse-light',
+      dark: 'vitesse-dark'
+    }
+  },
+  themeConfig: {
+    logo: '/logo.svg',
+
+    socialLinks: [{ icon: 'github', link: github }],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright:
+        'Copyright © 2021-PRESENT Anthony Fu, Matías Capeletto and DataView contributors'
+    },
+
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+      { text: 'API', link: '/api/' },
+      { text: 'Config', link: '/config/' },
+      { text: 'Advanced', link: '/advanced/index' },
+      {
+        text: `v${version}`,
+        items: [
+          {
+            text: 'Release Notes ',
+            link: releases
+          }
+        ]
+      }
+    ],
+
+    sidebar: {
+      '/advanced': [
+        {
+          text: 'Advanced',
+          items: [
+            {
+              text: 'DataView Node API',
+              link: '/advanced/index'
+            }
+          ]
+        }
+      ],
+      '/': [
+        {
+          text: 'Guide',
+          items: [
+            {
+              text: 'Getting Started',
+              link: '/guide/'
+            }
+          ]
+        },
+        {
+          text: 'API',
+          items: [
+            {
+              text: 'Test API Reference',
+              link: '/api/'
+            }
+          ]
+        },
+        {
+          text: 'Config',
+          items: [
+            {
+              text: 'Config Reference',
+              link: '/config/'
+            }
+          ]
+        }
+      ]
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+layout: home
+sidebar: false
+
+title: DataView
+titleTemplate: A blazing fast unit test framework powered by Vite
+
+hero:
+  name: DataView
+  text: ZGT DataView Framework
+  tagline: A Vite-native unit test framework. It's fast!
+  image:
+    src: /logo-shadow.svg
+    alt: DataView
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/DataView-dev/DataView
+
+features:
+  - title: Vite Powered
+    details: Reuse Vite's config, transformers, resolvers, and plugins - consistent across your app and tests.
+  - title: ESM, TypeScript, JSX
+    details: Out-of-box ESM, TypeScript and JSX support powered by esbuild
+---
