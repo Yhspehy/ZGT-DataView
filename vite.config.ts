fdf98c687bf5577d1ab9afdd492915d71edccc1d
Yhspehy <670395851@qq.com>
2023-03-03 15:05:14 +0800

    chore: remove rollup-plugin-gzip

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import gzipPlugin from 'rollup-plugin-gzip'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), gzipPlugin()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
